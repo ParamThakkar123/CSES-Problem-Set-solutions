eb8a2177982ff25ea3ac1c2069fe06761c9f1d7b
Param Thakkar <128291516+ParamThakkar123@users.noreply.github.com>
2024-04-07 09:41:47 +0530

    Create Missing Coin Sum.cpp

---
+++ Sorting and searching /Missing Coin Sum.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int n;
+    cin >> n;
+ 
+    vector<int> arr(n);
+ 
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+ 
+    sort(arr.begin(), arr.end());
+    long long res = 1;
+    for(int i = 0; i < n && arr[i] <= res; i++){
+        res += arr[i];
+    }
+ 
+    cout << res << endl;
+}
